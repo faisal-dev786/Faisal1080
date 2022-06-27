032c7db52ecec7ad1b66c7411a0badcfc3b59c3d
fasi1080 <fasinadeem108@gmail.com>
2022-06-27 13:37:25 +0500

    updated

---
+++ docs/app.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+    var typed6 = new Typed('#status', {
+        strings: ["", "" +
+        "Connecting to server.^700.^700.^700.^700.^700. Success.<br>" +
+        "^2000Fetching information^700.^700.^700.^700.^700. Success.<br>" +
+        "^1700Name: Faisal Nadeem<br>" +
+        "^1700Email: <a href='mailto:fasinadeem108@gmail.com'>fasinadeem108@gmail.com</a><br>" +
+        "^1700Phone: +92 3076169088<br>" +
+        "^1700Linkedin: <a href='https://www.linkedin.com/in/faisal-nadeem-2063b3160/'>linkedin.com/in/faisal-nadeem/</a><br>" +
+        "^1700GitHub: <a href='https://github.com/Faisal1080/'>Faisal1080</a><br>" +
+        "^1700Twitter: <a href='https://twitter.com/Faisal_10800'>@Faisal_10800</a><br>" +
+        "^1700Availability: Monday to Friday, From 10:00 AM to 04:00 PM (GMT+5)<br>" +
+        "^1700Want to discuss something? <a href='https://www.linkedin.com/in/Faisal Nadeem/'>Let's connect</a> to have a chat!"
+        ],
+        typeSpeed: 60,
+        loop: false
+    });
+}, 2000);
